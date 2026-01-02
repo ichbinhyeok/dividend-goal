404ea541bccc0fe13e94b42e4f2774404fe4a148
shinhyeok <shinhyeok22@gmail.com>
2026-01-02 17:42:49 +0900

    init

---
+++ src/test/java/org/example/dividendgoal/DividendGoalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dividendgoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DividendGoalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
